cf4904f78318d5c42ba96e4c47062ab2823416a5
Peter Surowski <Peter Surowski>
2017-09-24 22:45:52 -0700

    Added more files.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,7 @@
+// Dependencies:
+var express = require("express");
+var burger = require("burger.js");
+
+// Create a router:
+
+// Export the router:
---
+++ models/burger.js
@@ -0,0 +1,7 @@
+// Dependencies:
+var orm = require("/orm.js");
+
+// Create code that will call orm functions using 
+// burger-specific input from the orm.
+
+module.exports = burger;
